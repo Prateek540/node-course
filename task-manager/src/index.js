9d41e30a98db9434c509ee8230dd245472b7fefb
Prateek <prateekpathak97@gmail.com>
2023-02-23 02:33:42 +0530

    commit22Feb

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 require("./db/mongoose");
-
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = 3000;
-
-// app.use((req, res, next) => {
-//   res.send("Server is under maintenance. Try again later!!!");
-// });
 
 app.use(express.json());
 app.use(userRouter);
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -50,6 +50,9 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
+    avatar: {
+      type: Buffer,
+    },
   },
   {
     timestamps: true,
@@ -67,6 +70,7 @@ userSchema.methods.toJSON = function () {
   const userObject = user.toObject();
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
   return userObject;
 };
 
